0773e5b3d19ad680561c5921ba7eba531d62ca84
Arthur.Ribeiro <63483502+arthurfrp@users.noreply.github.com>
2021-12-17 15:22:09 -0300

    Fix pageSlug error
    HTTP 400 httpBadRequest: Unable to parse value of "$pageSlug=undefined". Please quote string values.

--- components/Review.js
+++ components/Review.js
@@ -4,10 +4,10 @@ const Review = ({ review }) => {
 
   return (
     <div className="review-box">
-      <h1>{review.rating}</h1>
-      <h2>{review.traveller.name}</h2>
+      <h1>{review?.rating}</h1>
+      <h2>{review?.traveller?.name}</h2>
       <img
-        src={urlFor(review.traveller.image)
+        src={urlFor(review?.traveller?.image)
           .width(50)
           .height(50)
           .crop("focalpoint")
